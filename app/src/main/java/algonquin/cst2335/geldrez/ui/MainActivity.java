56b6c1250b71e8d01c57836b172afee2c1ea7ba8
Alexander Geldrez <geld0011@algonquinlive.com>
2024-01-25 20:40:18 -0500

    Lab 2 done

---
+++ app/src/main/java/algonquin/cst2335/geldrez/ui/MainActivity.java
@@ -0,0 +1,67 @@
+package algonquin.cst2335.geldrez.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+import algonquin.cst2335.geldrez.data.MainViewModel;
+import algonquin.cst2335.geldrez.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private MainViewModel model;
+    private ActivityMainBinding variableBinding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        model = new ViewModelProvider(this).get(MainViewModel.class);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+
+        variableBinding.mybutton.setOnClickListener(click ->
+        {
+            model.editString.postValue(variableBinding.myedittext.getText().toString());
+        });
+        model.editString.observe(this, s -> {
+            variableBinding.textview.setText("Your edit text has: " + s);
+        });
+
+        model.isSelected.observe(this, selected -> {
+            variableBinding.checkBox.setChecked(selected);
+            variableBinding.radioButton.setChecked(selected);
+            variableBinding.switch1.setChecked(selected);
+
+            String message = "The value is now: " + selected;
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        });
+        variableBinding.checkBox.setOnCheckedChangeListener((checkBox, isChecked)-> {
+            model.isSelected.postValue(variableBinding.checkBox.isChecked());
+        });
+
+        variableBinding.radioButton.setOnCheckedChangeListener((radioButton, isChecked)-> {
+            model.isSelected.postValue(variableBinding.radioButton.isChecked());
+        });
+
+        variableBinding.switch1.setOnCheckedChangeListener((switch1, isChecked)-> {
+            model.isSelected.postValue(variableBinding.switch1.isChecked());
+        });
+
+        variableBinding.picture.setOnClickListener(view -> {
+            Toast.makeText(getApplicationContext(), "ImageView clicked", Toast.LENGTH_SHORT).show();
+        });
+
+        variableBinding.myimagebutton.setOnClickListener(view -> {
+            int width = view.getWidth();
+            int height = view.getHeight();
+            String message = "The width = " + width + " and height = " + height;
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+}
