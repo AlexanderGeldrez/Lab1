b52838a40694394b0c91d8194ae842d91dc5fef5
Alexander Geldrez <geld0011@algonquinlive.com>
2024-02-12 20:00:30 -0500

    Lab4 done

--- app/src/main/java/algonquin/cst2335/geldrez/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/geldrez/MainActivity.java
@@ -1,41 +1,105 @@
 package algonquin.cst2335.geldrez;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.Switch;
+
+import java.io.File;
+
+import algonquin.cst2335.geldrez.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imgView;
-    Switch sw;
-    @SuppressLint("MissingInflatedId")
+
+    private static String TAG = "MainActivity";
+
+    protected ActivityMainBinding binding;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        imgView = findViewById(R.id.flagView);
-        sw = findViewById(R.id.spin_switch);
+        Log.w(TAG, "In onCreate() - Loading Widgets");
 
-        sw.setOnCheckedChangeListener((btn,isChecked) ->{
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
+        // Where can you save files:
+        File myDir = getFilesDir();
+        String path = myDir.getAbsolutePath();
 
-                imgView.startAnimation(rotate);
+        SharedPreferences prefs = getSharedPreferences("MyFile", Context.MODE_PRIVATE);
 
-            } else {
-                imgView.clearAnimation();
-            }
+        // get an Editor
+        SharedPreferences.Editor edit = prefs.edit();
+
+        edit.putInt("Age",27);
+        edit.putString("NAME","Peter");
+
+        // save to disk
+        edit.commit();
+
+
+        binding.loginButton.setOnClickListener(clk -> {
+            Log.d(TAG, "You clicked the button");
+
+            // Code to be executed when the login button is clicked
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+
+            String whatIsTyped = binding.emailText.getText().toString();
+
+            nextPage.putExtra("Email", whatIsTyped);
+            nextPage.putExtra("AGE", 26);
+            nextPage.putExtra("DAY", "Sunday");
+
+            startActivity(nextPage);
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG, "In onStart() - Loading Widgets");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w(TAG, "In onResume() - Loading Widgets");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG, "In onPause() - Loading Widgets");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w(TAG, "In onStop() - Loading Widgets");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w(TAG, "In onDestroy() - Loading Widgets");
     }
 }
---
+++ app/src/main/java/algonquin/cst2335/geldrez/SecondActivity.java
@@ -0,0 +1,85 @@
+package algonquin.cst2335.geldrez;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import algonquin.cst2335.geldrez.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private ActivitySecondBinding binding;
+    private ImageView profileImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent fromPrevious = getIntent();
+
+        // Get values from the intent extras
+        String email = fromPrevious.getStringExtra("Email");
+        String day = fromPrevious.getStringExtra("DAY");
+        int age = fromPrevious.getIntExtra("AGE", 0);
+
+        // Set the text in the TextView
+        binding.textView.setText("Welcome back " + email + " and " + day + " and " + age);
+
+        // Set a click listener for the "Go Back" button
+        binding.goBackButton.setOnClickListener(clk -> {
+            finish();
+        });
+
+        // Find the ImageView by its ID
+        profileImage = findViewById(R.id.imageView);
+
+        // Start dialing a phone number
+        binding.callNumber.setOnClickListener( clk -> {
+            String phoneNumber = binding.callNumber.getText().toString();
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel:" + phoneNumber));
+            startActivity(call);
+        });
+
+        // Launch the camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                                // Only start the camera if permission is granted
+                                startActivity(cameraIntent);
+                            } else {
+                                // Request camera permission
+                                requestPermissions(new String[]{Manifest.permission.CAMERA}, 20);
+                            }
+
+                            // Get the captured image from the result
+                            Intent data = result.getData();
+                            Bitmap thumbnail = data.getParcelableExtra("data");
+                            // Set the captured image to the ImageView
+                            profileImage.setImageBitmap(thumbnail);
+                        }
+                    }
+                });
+        // Launch the camera activity
+        cameraResult.launch(cameraIntent);
+    }
+}
